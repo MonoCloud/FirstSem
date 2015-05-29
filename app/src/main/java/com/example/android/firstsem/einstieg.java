e05a10d9b9e2ac25d0262c245fdb9164e87ca5a6
ln047 <ln047@uni-rostock.de>
2015-05-29 18:29:59 +0200

    Bla bla bla

--- app/src/main/java/com/example/android/firstsem/einstieg.java
+++ app/src/main/java/com/example/android/firstsem/einstieg.java
@@ -1,47 +1,24 @@
 package com.example.android.firstsem;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class einstieg extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_einstieg);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_einstieg, menu);
         return true;
-    }
-
-    public void switchToEinstieg(View view){
-        startActivity(new Intent(MainActivity.this, einstieg.class));
-    }
-
-    public void switchToCampus(View view){
-        startActivity(new Intent(MainActivity.this, campus.class));
-    }
-
-    public void switchToTermin(View view){
-        startActivity(new Intent(MainActivity.this, termine.class));
-    }
-
-    public void switchToMensa(View view){
-        startActivity(new Intent(MainActivity.this, mensa.class));
-    }
-
-    public void switchToMap(View view){
-        startActivity(new Intent(MainActivity.this, map.class));
     }
 
     @Override
